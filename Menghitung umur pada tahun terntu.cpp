c75f3fd70d118dcf69c7206d3eb3186b4ca22bdd
Anugrah Bimantara <anugrahbima663@gmail.com>
2023-07-01 01:42:27 +0700

    -

---
+++ Menghitung umur pada tahun terntu.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int tahunLahir, tahunTujuan;
+
+    // Mengimputkan tahun kelahiran
+    cout << "Masukkan tahun kelahiran Anda: ";
+    cin >> tahunLahir;
+
+    // Mengimputkan tahun tujuan untuk menghitung umur
+    cout << "Masukkan tahun tujuan: ";
+    cin >> tahunTujuan;
+
+    // Mendapatkan tahun saat ini
+    time_t now = time(0);
+    tm* saatIni = localtime(&now);
+    int tahunSaatIni = 1900 + saatIni->tm_year;
+
+    // Menghitung umur
+    int umur = tahunTujuan - tahunLahir;
+
+    // Memeriksa apakah sudah melewati hari ulang tahun di tahun tujuan
+    if ((saatIni->tm_mon + 1) < saatIni->tm_mon ||
+        ((saatIni->tm_mon + 1) == saatIni->tm_mon && saatIni->tm_mday < saatIni->tm_mday)) {
+        umur--;
+    }
+
+    // Output hasil
+    cout << "Umur Anda pada tahun " << tahunTujuan << " adalah: " << umur << " tahun" << endl;
+
+    return 0;
+}
+
